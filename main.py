a08715570fd6ad501ec2ebf213c1eb6751ce58be
Peter C <30731499+codeybear@users.noreply.github.com>
2022-09-28 16:50:32 +0100

    Depth first

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from termios import TABDLY
+
+
 class Node:
-  def __init__(self, value):
+  def __init__(self, value=0):
     self.value = value
     self.left = None
     self.right = None
@@ -9,8 +12,18 @@ class Tree:
   def __init__(self):
     self.root = None
 
+  def calc_all_paths(self, node, target, paths):
+    target = target - node.value
+    paths.append[node.value]
 
-  def calc(self, node, target):
+    if node.left:
+      self.calc_all_paths(node.left, target, paths)
+
+    if node.right:
+      self.calc_all_paths(node.right, target, paths)
+
+
+  def calc_target(self, node, target):
     target = target - node.value
     result = 0
 
@@ -21,10 +34,10 @@ class Tree:
       return 0
 
     if node.left:
-      result = self.calc(node.left, target)
+      result = self.calc_target(node.left, target)
 
     if node.right:
-      result += self.calc(node.right, target)
+      result += self.calc_target(node.right, target)
 
     return result
 
@@ -36,4 +49,4 @@ tree.root.left.left = Node(9)
 tree.root.right.left = Node(10)
 tree.root.right.right = Node(5)
 
-print(tree.calc(tree.root, 23))
+print(tree.calc_all_paths(tree.root, 23, []))
