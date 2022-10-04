825ac1d4559077da74232bd5d81ca46dd09f0abe
Peter C <30731499+codeybear@users.noreply.github.com>
2022-10-04 14:51:22 +0100

    Binary search with ceiling

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,6 +1,5 @@
-'''Binary search that works for ascending order'''
-
 def binary_search(arr, num):
+    '''Binary search that works for ascending order'''
     start = 0
     end = len(arr) - 1
 
@@ -15,6 +14,32 @@ def binary_search(arr, num):
         else:
             end = mid - 1
 
-
 print(binary_search([4, 6, 10], 10))
 print(binary_search([1, 2, 3, 4, 5, 6, 7], 5))
+
+
+def binary_search_ceiling(arr, num):
+    '''Binary search for the smallest number greater than or equal to'''
+    start = 0
+    end = len(arr) - 1
+    smallest = float('inf')
+
+    if arr[len(arr) - 1] < num:
+        return -1
+
+    while start <= end:
+        mid = start + (end - start) // 2
+
+        if arr[mid] == num:
+            return mid
+        
+        if num > arr[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+            smallest = min(smallest, arr[mid])
+
+    return smallest
+
+print(binary_search_ceiling([1, 3, 8, 10, 15], 12))
+print(binary_search_ceiling([4, 6, 10], 17))
