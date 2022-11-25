3f0eb00211dcc9ef7a834916ef9f284cd3f8ded6
Peter C <30731499+codeybear@users.noreply.github.com>
2022-11-25 14:48:50 +0000

    Clean up code

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ https://www.geeksforgeeks.org/coin-change-dp-7
 '''
 
 def coin_change(coins, used, amount):
-  combo_count = 0
-  
   for coin in coins:
-    used_new = []
-    used_new.extend(used)
+    used_new = used[:]
     used_new.append(coin)
+
     amount_new = amount - coin
-    
+
     if amount_new == 0: 
       print(used_new)
       return 1
 
     if amount_new < 0:
       return 0
-    
+
+    combo_count = 0
     combo_count += coin_change(coins, used_new, amount_new)
 
   return combo_count
 
-print(coin_change([2, 5, 3, 6], [], 10))
+# print(coin_change([2, 5, 3, 6], [], 10))
+print(coin_change([1, 2, 3], [], 4))
