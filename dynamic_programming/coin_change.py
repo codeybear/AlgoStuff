593df2d2c2816c51c67bdc9b5d71846aed1d50b0
Peter C <30731499+codeybear@users.noreply.github.com>
2022-12-06 15:28:06 +0000

    Coin change completed

--- dynamic_programming/coin_change.py
+++ dynamic_programming/coin_change.py
@@ -1,12 +1,13 @@
 '''
 Dynamic programming coin change problem
 
-First attempt that needs to check for duplicates.
-
 https://www.geeksforgeeks.org/coin-change-dp-7
 '''
 
 def coin_change(coins, used, amount):
+    '''
+    First attempt that needs to check for duplicates.
+    '''
     combo_count = 0
     
     for coin in coins:
@@ -27,5 +28,38 @@ def coin_change(coins, used, amount):
 
     return combo_count
 
-print(coin_change([2, 5, 3, 6], [], 10))
+# print(coin_change([2, 5, 3, 6], [], 10))
 # print(coin_change([1, 2, 3], [], 4))
+
+
+def coin_change_correct(coins, n, sum): 
+    '''
+    Coin change dp solution.
+
+    Copied from geeks4geeks and tidied up
+
+    Uses the include/exclude method which is also given by the knapsack solution
+
+    This method descends through number of items - including and excluding each item
+    This means you will get 4p = 2,1,1 but not 1,2,1 or 1,1,2 i.e. combinations no permutations
+    '''
+    # If sum is 0 then there is 1 solution (do not include any coin)
+    if (sum == 0):
+        return 1
+ 
+    # If sum is less than 0 then no solution exists
+    if (sum < 0):
+        return 0
+ 
+    # If there are no coins and sum is greater than 0, then no solution exist
+    if (n <= 0):
+        return 0
+ 
+    # count is sum of solutions (i)
+    return coin_change_correct(coins, n - 1, sum) + coin_change_correct(coins, n, sum-coins[n-1])
+
+ 
+# Driver program to test above function
+coins = [1, 2, 3]
+n = len(coins)
+print(coin_change_correct(coins, n, 4))
