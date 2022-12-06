b658a8f1c830a090ba46705929660fb156785898
Peter C <30731499+codeybear@users.noreply.github.com>
2022-12-06 14:55:41 +0000

    Commenting coin change dp

--- main.py
+++ main.py
@@ -4,29 +4,23 @@ Coin change dp solution
 Uses the include/exclude method which is also given by knapsack
 '''
 
-
-def count(coins, n, sum):
- 
-    # If sum is 0 then there is 1
-    # solution (do not include any coin)
+def count(coins, n, sum): 
+    # If sum is 0 then there is 1 solution (do not include any coin)
     if (sum == 0):
         return 1
  
-    # If sum is less than 0 then no
-    # solution exists
+    # If sum is less than 0 then no solution exists
     if (sum < 0):
         return 0
  
-    # If there are no coins and sum
-    # is greater than 0, then no
+    # If there are no coins and sum is greater than 0, then no
     # solution exist
     if (n <= 0):
         return 0
  
     # count is sum of solutions (i)
-    # including coins[n-1] (ii) excluding coins[n-1]
     return count(coins, n - 1, sum) + count(coins, n, sum-coins[n-1])
- 
+
  
 # Driver program to test above function
 coins = [1, 2, 3]
